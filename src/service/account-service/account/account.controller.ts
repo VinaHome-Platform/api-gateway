d8143b5fbd8da843225ef4bc3b443aa4b0da540c
tuanthanh2603 <tuanthanhktltk21@gmail.com>
2025-05-19 23:33:39 +0700

    custom exception

--- src/service/account-service/account/account.controller.ts
+++ src/service/account-service/account/account.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { Body, Controller, Post } from '@nestjs/common';
 import { AccountService } from './account.service';
 import { CreateAccountStaff } from './account.dto';
@@ -7,7 +8,7 @@ export class AccountController {
   constructor(private readonly accountService: AccountService) {}
 
   @Post('/create-account-staff')
-  createAccountStaff(@Body() data: CreateAccountStaff) {
+  async createAccountStaff(@Body() data: CreateAccountStaff) {
     return this.accountService.createAccountStaff(data);
   }
 }
--- src/service/account-service/account/account.service.ts
+++ src/service/account-service/account/account.service.ts
@@ -1,14 +1,34 @@
-import { Inject, Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { CreateAccountStaff } from './account.dto';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class AccountService {
   constructor(
     @Inject('ACCOUNT_SERVICE') private readonly client: ClientProxy,
   ) {}
-  createAccountStaff(data: CreateAccountStaff) {
-    console.log('createAccountStaff', data);
-    // return this.client.send({ ac: 'create_account_staff' }, data);
+
+  async createAccountStaff(data: CreateAccountStaff) {
+    try {
+      console.log(data);
+      const result = await firstValueFrom(
+        this.client.send({ ac: 'create_account_staff' }, data),
+      );
+      return result;
+    } catch (error) {
+      throw new InternalServerErrorException({
+        success: false,
+        message: 'Máy chủ dịch vụ không phản hồi',
+      });
+    }
   }
 }
